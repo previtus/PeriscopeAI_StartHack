cbd8315e2da041ce83395228a05142fd378a8321
previtus <previtus@gmail.com>
2019-03-09 13:45:54 +0100

    vague kotlin attempt

---
+++ kotlin_build.sh
@@ -0,0 +1,2 @@
+kotlinc kotlin_test1.kt -include-runtime -d kotlin_test1.jar
+java -jar kotlin_test1.jar
---
+++ kotlin_test1.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+  println("Hello World")
+}
+
+
+// build and run with:
+// kotlinc kotlin_test1.kt -include-runtime -d kotlin_test1.jar
+// java -jar kotlin_test1.jar
